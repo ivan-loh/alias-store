0cf66e92fd701d0b6969bc005f86788ce7ff6373
ivan <ivan@eriad.com>
2020-11-10 10:30:20 +0800

    ip response check

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -4,7 +4,7 @@ const express = require('express');
 const router  = express.Router();
 
 router.get('/', (req, res) => {
-  res.jsonp({message:'what up ?'});
+  res.jsonp({message:'what up ?', ip: req.ip});
 });
 
 module.exports = router;
